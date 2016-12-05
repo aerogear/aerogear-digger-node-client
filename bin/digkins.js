8d529673fbe0514e7098e6b9634d984d982a77cd
Wojciech Trocki <wtrocki@redhat.com>
2016-12-05 14:25:18 +0000

    Fixed eslint issues

--- bin/digkins.js
+++ bin/digkins.js
@@ -1,16 +1,17 @@
 #!/usr/bin/env node
 
-var yargonaut = require('yargonaut')
+require('yargonaut')
   .style('red')
   .helpStyle('green')
-  .errorsStyle('red')
+  .errorsStyle('red');
+
 var yargs = require('yargs');
 
 yargs.commandDir("../lib/commands")
-    .help('help')
-    .alias('h', 'help')
-    .strict(true)
-    .demand(1)
-    .version()
-    .completion()
-    .argv
+  .help('help')
+  .alias('h', 'help')
+  .strict(true)
+  .demand(1)
+  .version()
+  .completion()
+  .argv;
